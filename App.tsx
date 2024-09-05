367902ebcda97232708a35d78075215984ebc95f
daviaxs <cadwvk@gmail.com>
2024-09-05 14:31:33 -0300

    feat: Add click event listener for OneSignal push notification

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { OneSignal } from "react-native-onesignal"
+import { NotificationClickEvent, OneSignal } from "react-native-onesignal"
 
 import { Routes } from './src/routes';
 
@@ -10,6 +10,7 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 import { tagUserIndoCreate } from './src/notifications/notificationTags';
+import { useEffect } from 'react';
 
 OneSignal.initialize("6728ceec-4eb0-4b7d-9951-2ec266333718")
 OneSignal.Notifications.requestPermission(true)
@@ -18,6 +19,24 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserIndoCreate()
+
+  useEffect(() => {
+    const handleNotificationClick = (event: NotificationClickEvent): void => {
+      console.log("Notificação aberta")
+    }
+
+    OneSignal.Notifications.addEventListener(
+      "click",
+      handleNotificationClick
+    )
+
+    return () => {
+      OneSignal.Notifications.removeEventListener(
+        "click",
+        handleNotificationClick
+      )
+    }
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
